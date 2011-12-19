fa24694f0c85000c06de08dce7610d595ba168f2
unknown <JoeyYao@.(none)>
2011-12-19 17:39:50 +0800

    Init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def go
+  end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opplink::Application.config.secret_token = '42817e12c6615342897e7c428f2dbdc36ec69e92ddd872a89f5b51f561539e82f506aa86a22bac9a1fee499b20fe58d8cd40d416e414e37871582010ba906ad8'
